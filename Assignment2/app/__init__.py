7d913e730f95fc237887eeae8fb61e920b6765f1
LCF2399 <150310541+LCF2399@users.noreply.github.com>
2024-10-18 13:46:30 -0400

    Added a secret key

--- Assignment2/app/__init__.py
+++ Assignment2/app/__init__.py
@@ -4,11 +4,20 @@
 # To import the flask into the project use the following line:
 from flask import Flask
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 flask_app = Flask(__name__)
 
+db_username = os.environ["MONGODB_USERNAME"]
+db_password = os.environ["MONGODB_PASSWORD"]
+
+print(db_username)
+print(db_password)
+
 # MongoDB Atlas Connection
-client = MongoClient("mongodb+srv://root:9xgCOMv6ZbMtpPKH@cluster0.kaq1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.kaq1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.shop_db  # Replace "app" with your database name
 products_collection = db.products  # Replace products with your collection name
 
